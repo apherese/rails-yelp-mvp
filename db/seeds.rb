d5ddda6c2a6b93043eade8fcb3eb1aea2ff2b0c6
jean-pierre daverio <jeanpierre.daverio@orange.fr>
2023-01-04 19:49:58 +0100

    migration done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "12345678", category: "japanese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "98765678", category: "italian"}
+septime = {name: "Septime", address: "22 rue du cherche-midi, 75006 Paris", phone_number: "12388678", category: "french"}
+pause_cafe = {name: "Pause-Café", address: "6 rue traversière, 75012 Paris", phone_number: "12388998", category: "french"}
+shokudo = {name: "Jinchan Shokudo", address: "55 rue du faubourg St Antoine, 75012 Paris", phone_number: "17778998", category: "chinese"}
+
+[dishoom, pizza_east, septime, pause_cafe, shokudo].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
